5d6f8ede5ca040b57ecfbf12c2401c13e79c46a8
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2024-10-15 11:36:44 -0300

    on dbl click aula js

---
+++ ondblclick.js
@@ -0,0 +1,8 @@
+
+
+
+//DIFERENTE DE ONCLICK, O ONDBLCLICK PRECISA DE DUPLO CLIQUE PARA SER ATIVADO
+// lEMBRANDO QUE NO HTML TEM QUE ESTAR O MESMO NOME NO BUTTON... EX:   <button ondblclick="eventoDblClick()">Clique aqui</button>
+function eventoDblClick () {
+    alert('Acionou um evento de click DUPLO');
+}
