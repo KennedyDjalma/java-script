a5688d3a1ac438a68b45dff51f272a6676aea112
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-04-23 11:34:00 -0300

    if e else simples e composto

---
+++ js/a11ex009/ex009.js
@@ -0,0 +1,8 @@
+//VARIAVEL SIMPLES
+
+var vel = 60.2
+console.log(`A velocidade do seu carro é: ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO.')
+} 
+ console.log('Dirija com cuidado')
