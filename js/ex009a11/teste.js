5e286a6685c73c948f5626ca67fd39662b757642
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-05-28 18:49:40 -0300

    treino condiçoes

---
+++ js/ex009a11/teste.js
@@ -0,0 +1,14 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} 
+else {
+    console.log(`${vel} KM/H VELOCIDADE PERMITIDA`)
+}
+
+console.log(`Dirija com segurança`)
+
+
+
+
