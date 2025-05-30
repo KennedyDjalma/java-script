672b71e0b86fff3b1cdb80f9c160accd4c8171f1
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-05-30 18:36:06 -0300

    ajustes

--- js/ex010/ex013.js
+++ js/ex010/ex013.js
@@ -36,24 +36,24 @@ else if (diaSemana == 6) {
 }
 */
 console.log(diaSemana)
-switch ( diaSemana ) {
+switch (diaSemana) {
     case 0:
         console.log('Domingo')
         break
     case 1:
-        console.log('Segunda')
+        console.log('Segunda-feira')
         break
     case 2:
-        console.log('Terça')
+        console.log('Terça-feira')
         break
     case 3:
-        console.log('Quarta')
+        console.log('Quarta-feira')
         break
     case 4:
-        console.log('Quinta')
+        console.log('Quinta-feira')
         break
     case 5:
-        console.log('Sexta')
+        console.log('Sexta-feira')
         break
     case 6:
         console.log('Sabado')
