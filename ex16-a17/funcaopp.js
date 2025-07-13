3650a9d3354c878e0fbd8d82d1c236a5e8f3248f
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-07-13 11:24:15 -0300

    profição programador aula de função

---
+++ ex16-a17/funcaopp.js
@@ -0,0 +1,18 @@
+ //Declara a função
+function minhaFuncao() {
+    console.log('Acionou a função')
+}
+//Aciona a função
+minhaFuncao()
+
+
+/*
+//Declara a função
+function minhaFuncao(messege, userName) {
+    console.log(message, userName);
+}
+//Aciona a função
+minhaFuncao('Função1');
+minhaFuncao('Função2');
+minhaFuncao('Função3');
+*/
