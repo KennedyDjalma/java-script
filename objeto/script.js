6244d359d87a1b08579cc7e1bc240445ab70c134
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-07-14 11:10:36 -0300

    aula funcao

---
+++ objeto/script.js
@@ -0,0 +1,49 @@
+/*
+const Kennedy = {
+    completeName: 'Kennedy Djalma de Santana',
+    idade: 29,
+    showMessage: function(){
+        alert('Menssagem de alerta')
+    },
+    estado: 'Pernambuco'
+}
+
+console.log(Kennedy.showMessage() )
+*/
+
+/*
+const Kennedy = {
+    completName: 'Kennedy Djalma de Santana',
+    idade: 29,
+    showMessage: function(){
+        alert('Mensagem de alerta 2')
+    },
+    estado:'Pernambuco',
+    cidade: 'Lagoa do Itaenga'
+}
+
+console.log(Kennedy.showMessage())
+*/
+
+
+/*
+const Kennedy = {
+    completeName: 'Kennedy Djalma de Santana',
+    idade: 29,
+    showMessage: function(){
+        alert('MENSAGEM DE ALERTA TESTE')
+    }
+}
+console.log(Kennedy.showMessage())
+*/
+
+const Kennedy = {
+
+    completeName: 'Kennedy Djalma de Santana.',
+    idade: 29,
+    showMessage: function(){
+        alert('MENSAGEM DE ALERTA TESTE 2')
+    },
+    estado: 'Pernambuco'
+}
+console.log(Kennedy.showMessage())
