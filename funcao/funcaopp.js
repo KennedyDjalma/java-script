bbeb9271916ffaf2e0a2ae9a80809cc3869375df
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-07-14 17:36:17 -0300

    teste função console

--- funcao/funcaopp.js
+++ funcao/funcaopp.js
@@ -1,10 +1,11 @@
- //Declara a função
+/*
+//Declara a função
 function minhaFuncao() {
     console.log('Acionou a função')
 }
 //Aciona a função
 minhaFuncao()
-
+*/
 
 /*
 //Declara a função
@@ -16,3 +17,26 @@ minhaFuncao('Função1');
 minhaFuncao('Função2');
 minhaFuncao('Função3');
 */
+
+/*
+function primeiraFuncao() {
+    alert('PRIMEIRO ALERTA FUNÇÃO')
+    console.log('PRIMEIRO ALERTA CONSOLE')
+}
+
+console.log(primeiraFuncao())
+minhaFuncao()
+*/
+
+/*
+function minhaFuncao() {
+    console.log(  'Função de teste console'  )
+}
+minhaFuncao()
+*/
+
+function minhaFuncao(){
+    console.log ('Teste de Função no console')
+}
+
+minhaFuncao()
