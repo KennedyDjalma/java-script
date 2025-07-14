cd24cee07acdf78e6eaf79671b1caf6e6d58ffe6
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-07-14 17:46:14 -0300

    teste de vetor e matriz

--- array/script.js
+++ array/script.js
@@ -1,3 +1,4 @@
+/*
 const vetor = ['Kennedy','Djalma','Santana'];
 
 const matriz = [
@@ -9,3 +10,13 @@ const matriz = [
 console.log (matriz)
 //console.log (vetor[2])
 //console.log (matriz[2])
+*/
+
+const vetor = ['K', 'E', 'N']
+
+const matriz = [
+    ['carro0','carro1','carro2'],
+    ['pessoa0','pessoa1','pessoa2'],
+    ['lugar0','lugar1','lugar2'],
+]
+console.log(matriz)
