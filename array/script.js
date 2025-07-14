7ead4216d8f35541bea68d2d1b5087fcbbb385a4
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-07-14 11:33:22 -0300

    aula array

---
+++ array/script.js
@@ -0,0 +1,11 @@
+const vetor = ['Kennedy','Djalma','Santana'];
+
+const matriz = [
+    ['carro0','carro1','carro2'],
+    ['pessoa0','pessoa1','pessoa2'],
+    ['lugar0','lugar1','lugar2'],
+]
+
+console.log (matriz)
+//console.log (vetor[2])
+//console.log (matriz[2])
