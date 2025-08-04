afcd278abc8fd8f6527524046025a6b49a45e722
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-08-04 18:24:11 -0300

    operador ternario

---
+++ operador-ternario/script.js
@@ -0,0 +1,6 @@
+const numero1 = 0;
+const numero2 = 2;
+
+//console.log(true ? 'OLÁ MUNDO! retornou verdadeiro' : 'retornou falso')
+
+numero1 ? alert ('retornou VERDADEIRO') : alert ('retornou FALSO')
