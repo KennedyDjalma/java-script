a2dafedae2c9ad9c2c719795c6c08e3205546d40
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-08-06 11:16:49 -0300

    Estrutura de Repetição

---
+++ estruturas-de-repetição/script.js
@@ -0,0 +1,48 @@
+// Estruturas de repetições ou Loops
+
+/*
+for (let numero = 0; numero <= 5; numero ++) {
+    console.log(`Repetição de múmero ${numero}`)
+}
+
+*/
+
+/*
+for (let numero = 0; numero <= 200; numero ++) {
+    console.log(`Repetição de múmero ${numero}`)
+    if (numero == 50){
+        break
+    }
+}
+*/
+
+/*
+//for in
+const object = {
+    nome : 'Kennedy',
+    idade : 29 ,
+    cidade : 'Lagoa' ,
+    veiculo : 'Moto' ,
+}
+
+for (key in object) {
+    console.log(object[key])
+}
+*/
+
+
+/*
+//for off
+const arrays = ['Honda', 'Yamaha', 'Dafra']
+
+for(item of arrays) {
+    console.log(item)
+}
+*/
+
+//while
+let numero = 0;
+while(numero <= 5){
+    console.log(numero);
+    numero ++
+}
