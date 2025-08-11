f85ea7863c45d68d41bd06d2c0b9e6a9612876fb
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-08-11 17:49:48 -0300

    aulas de js

--- desestruturacao/script.js
+++ desestruturacao/script.js
@@ -1,9 +1,37 @@
+
+//      DESESTRUTURAÇÃO DE OBJETOS
+/*
 const Pessoa = {
     nome : 'Kennedy',
     idade : 29 ,
     cidade : 'Lagoa',
 }
 
-const {} = Pessoa;
+const {cidade, nome, idade} = Pessoa;
 
-console.log(Pessoa)
+console.log()
+*/
+
+/*
+const Pessoa = {
+    nome : 'Kennedy',
+    idade : 29 ,
+    endereco: {
+        estado: 'PE',
+        cidade: 'Lagoa',
+        rua: 'Braz',
+        numero: 7
+    }
+}
+
+const {estado, cidade, rua, numero} = Pessoa.endereco;
+
+console.log(cidade)
+*/
+
+//      DESESTRUTURAÇÃO DE ARRAYS
+
+const motos = ['Honda', 'Yamaha', 'Dafra', 'Bajaj'];
+//console.log(motos[2])
+const [primeiraMoto, segundaMoto, terceiraMoto] = motos;
+console.log(primeiraMoto)  
---
+++ rest&spread/script.js
@@ -0,0 +1,31 @@
+/*
+//      REST
+function estados(...estados){
+    console.log(estados [1])
+}
+estados('CE', 'PE', 'BA', 'SP', 'RS', 'SC', )
+*/
+
+
+/*
+//       SPREAD
+const motosAltas = ['Bros', 'Crosser', 'Xre', 'Lander']
+const motosBaixas = ['Fan', 'Factor', 'Bis', 'Neo']
+
+const motos = [...motosAltas, ...motosBaixas]
+*/
+
+const pessoa = {
+    nome: ' Kennedy',
+    idade: 29,
+    profissao: 'ADS'
+}
+
+const endereco = {
+    estado: 'PE',
+    cidaed: 'Lagoa'
+}
+
+const dados = {
+    ...pessoa, ...endereco
+}
