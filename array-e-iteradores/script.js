1dfe4be52603b76410ccc832afa6c78cd905bacb
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-08-13 11:56:07 -0300

    array

---
+++ array-e-iteradores/script.js
@@ -0,0 +1,12 @@
+const estoque = ['arroz', 'feijão', 'arroz', 'macarrão', 'batata', '']
+const preços = [3.50, 5, 3.50, 2, 2.50, 3]
+const pessoas = [{nome:'kennedy', idade:29}, {nome:'santana', idade:92}]
+
+// forEach
+// map
+// reduce
+//find
+//findIndex
+// some
+// every
+// filter
