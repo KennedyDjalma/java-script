61982fd28ccb804dda503f35f9a5d09bd1841e09
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-10-12 10:17:07 -0300

    laços de repetiçoes

---
+++ estruturas-de-repetição/for/index.js
@@ -0,0 +1,8 @@
+/*
+for (let i=0; i<1001; i++) {
+document.getElementById('teste').innerHTML += i +', ';
+}
+*/
+for (let i=1800; i<=2025; i++){
+    document.getElementById('ano').innerHTML += '<option value=" '+ i + ' " > ' + i + ' </opition>';
+}
