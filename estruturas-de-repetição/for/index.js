8989d12d4692b8f775d11c56f77d43b3f9169e79
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-10-13 10:18:55 -0300

    aulas de laços de repetição

--- estruturas-de-repetição/for/index.js
+++ estruturas-de-repetição/for/index.js
@@ -1,8 +1,26 @@
 /*
-for (let i=0; i<1001; i++) {
-document.getElementById('teste').innerHTML += i +', ';
+for (let i = 0; i <= 2025; i++){
+    document.getElementById("teste").innerHTML += i + ", ";
 }
 */
+
+/*
 for (let i=1800; i<=2025; i++){
     document.getElementById('ano').innerHTML += '<option value=" '+ i + ' " > ' + i + ' </opition>';
+}
+*/
+
+/* --------- */
+var ano = new Date().getFullYear()
+
+for (let i = ano; i >= 1900; i--) {
+    document.getElementById("ano").innerHTML += "<option value=' "+ i +" '>" + i + " </opition> ";  
+}
+
+/* --------- */
+const carros = ["golf", "porshe", "mercedes", "ferrari"];
+var tamanho=carros.length;
+
+for (let i = 0; i < tamanho; i++) {
+    document.getElementById("teste").innerHTML += carros[i] + "-";
 }
