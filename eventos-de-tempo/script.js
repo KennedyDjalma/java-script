f72019d55781b49e8db3c5a8b5c77d1cdbf0080e
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-10-13 12:30:18 -0300

    eventos de tempo

---
+++ eventos-de-tempo/script.js
@@ -0,0 +1,22 @@
+function ativarContagem() {
+    /*
+    document.getElementById('tempo').innerHTML = "Começou a contar!";
+
+    tempo = setTimeout (function () {
+        document.getElementById('tempo').innerHTML = "Execultou o setTimeOut!";
+    }, 2000);
+    */
+   tempo = setInterval(function (){
+    var cronometro = document.getElementById('tempo').innerHTML;
+    var soma = parseInt(cronometro)+1;
+    document.getElementById('tempo').innerHTML = soma;
+   }, 1000);
+}
+
+function pararContagem() {
+    /*
+    clearTimeout(tempo);
+    document.getElementById('tempo').innerHTML = "Parou a contagem!";
+    */
+   clearInterval(tempo);
+}
