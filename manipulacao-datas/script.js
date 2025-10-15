ceb284fc88e309c2272ea10d8b7402e37f0154b6
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2025-10-15 18:03:11 -0300

    manipulação de datas

--- manipulacao-datas/script.js
+++ manipulacao-datas/script.js
@@ -6,9 +6,45 @@ console.log(data);
 
 // PEGA O ANO ATUAL COM 4 DIGITOS
 let ano = data.getFullYear();
-console.log(data);
+console.log(ano);
 
 
-// PEGAR O MES ATUAL
+// PEGAR O MES ATUAL - DE 0 QUE É JANEIRO ATÉ DEZEMBRO 11
 let mes = data.getMonth();
-console.log(data);
+console.log(mes);
+
+// MOSTRAR O MES NO FORMATO ESCRITO
+const mesesDoAno = ["Janeiro", "Fervereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+let mesEscrito = mesesDoAno[data.getMonth()];
+console.log(mesEscrito);
+
+// DIA DO MES - 1 ATÉ 31
+let diaMes = data.getDate();
+console.log(diaMes);
+
+// DIA DA SEMANA ESCRITO
+let diaSemana = data.getDay();
+const diaDaSemana = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sabado"];
+let diaEscrito = diaDaSemana[data.getDay()];
+console.log(diaEscrito);
+
+// USAR A HORA - DE 0 ATÉ 23
+let hora = data.getHours();
+console.log(hora);
+
+// USANDO OS MINUTOS
+let minutos = data.getMinutes();
+console.log(minutos);
+
+// USANDO OS SEGUNDOS
+let segundos = data.getSeconds();
+console.log(segundos);
+
+// USANDO OS MILISEGUNDOS
+let milisegundos = data.getMilliseconds();
+console.log(milisegundos);
+
+// DATA NO PADRAO BRASILEIRO
+let dataBr = data.toLocaleDateString();
+console.log(dataBr);
+
